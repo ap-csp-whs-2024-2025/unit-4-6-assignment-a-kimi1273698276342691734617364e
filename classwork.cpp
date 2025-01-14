03e0f82c73132acf57dd409e9ebea4afd1e05bd7
kimi1273698276342691734617364e <kimthai002@gmail.com>
2025-01-14 21:42:56 +0000

    wkjnf

--- classwork.cpp
+++ classwork.cpp
@@ -19,14 +19,41 @@
 
 // TODO: Create squareNum
 
+void squareNum(int n)
+{
+    std::cout << "The square of "<< n << " is "<< (n*n) << std::endl;
+}
+
 // TODO: Create distance
 
+void distance(double x, double y)
+{
+    double dist = abs(x - y);
+    std::cout << "The distance between " << x << " and " << y << " is " << dist << std::endl;
+}
 // TODO: Create rollDice
+
+void rollDice()
+{
+    int randomNumber = rand() % 6 +1;
+    std::cout << "you rolled a " << randomNumber << std::endl;
+}
+
 
 int main()
 {
     // You can follow along with any code in class here
+    squareNum (5);
+    squareNum (-5);
+    squareNum (0);
+    
+    std::cout << "Enter two numbers: \n";
+    std::cin >> num1 >> num2;
+    distance(num1, num2);
 
+    rollDice();
+    rollDice();
+    rollDice();
     
     return 0;
 }
